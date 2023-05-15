6ae88d0ae6a39b7cc77bcc98f249cd55a9d219e9
Dior2004 <kupaysinovdiyorbek@gmail.com>
2023-05-15 14:35:38 +0500

    🤩

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const jsonServer = require("json-server"); // importing json server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const midlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3500; // select port
 
 server.use(midlewares);
 server.use(router);
-
 server.listen(port);
