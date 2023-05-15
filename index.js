c328e44eccc1264ffdd3c615a9c2389da89656cd
Dior2004 <kupaysinovdiyorbek@gmail.com>
2023-05-15 14:02:27 +0500

    port change

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import jsonServer from "json-server"; // importing json server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const midlewares = jsonServer.defaults();
-const port = process.env.PORT || 3500;
+const port = process.env.PORT || 8080;
 
 server.use(midlewares);
 server.use(router);
+
 server.listen(port);
